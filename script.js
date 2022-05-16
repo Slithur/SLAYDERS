dcbf06197a00c822d42cb41181b50ee6799a7ce6
mahamedaden <joali218@gmail.com>
2022-05-15 22:27:22 -0500

    keys work

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ class Sprite {
     update() {
         this.draw()
         
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -62,6 +63,15 @@ velocity: {
 
 console.log(player)
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+
+}
 
     function animate() {
         window.requestAnimationFrame(animate)
@@ -69,11 +79,41 @@ console.log(player)
         c.fillRect(0, 0, canvas.width, canvas.height)
         player.update()
         enemy.update()
+
+        if (keys.a.pressed) {
+            player.velocity.x = -1
+    
+        } else if (keys.d.pressed) {
+            player.velocity.x = 1
+        }
     }
 
 animate();
 
 window.addEventListener('keydown', (event) => {
-console.log(event);
+    switch (event.key) {
+        case 'd' :
+        player.velocity.x = 1
+        break
+        case 'a' :
+        player.velocity.x =  -1
+        break
+        
+    }
+    console.log(event.key);
 
 })
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd' :
+        player.velocity.x = 0
+        break
+        case 'a' :
+            player.velocity.x = 0
+            break
+    }
+    console.log(event.key);
+
+})
+
